6aca595b2578c9848593c0a148d387e63c1e36a9
wujiaan2022 <emmawu2004@gmail.com>
2023-01-16 00:33:54 +0000

    Add code of two random numbers for the game question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,27 @@
+// wait for the DOM to finish loading before running the game
+// get the button element and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+
+    for(let button of buttons){
+        button.addEventListener("click", function(){
+            if(this.getAttribute("data-type") === "submit"){
+                alert("You clicked Submit!");
+            }else{
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+
 function runGame(){
 
+    // Create two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25 + 1);
+    let num2 = Math.floor(Math.random() * 25 + 1);
+    
 }
 function runGame(){
     
