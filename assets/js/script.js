5391fb4c81c578ec36671b3e57c343016a5fd067
wujiaan2022 <emmawu2004@gmail.com>
2023-01-16 04:56:14 +0000

    Add multiply function

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,7 +29,9 @@ function runGame(gameType){
 
     if (gameType === "addition"){
         displayAdditionQuestion(num1, num2);
-    }else{
+    }else if(gameType === "multiply"){
+        displayMultiplyQuestion(num1, num2);
+    } else{
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -70,7 +72,9 @@ function calculateCorrectAnswer(){
 
     if (operator === "+"){
         return [operand1 + operand2, "addition"];        
-    }else{
+    }else if (operator === "x"){
+        return [operand1 * operand2, "multiply"]; 
+    } else{
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -98,6 +102,7 @@ function incrementWrongAnswer(){
 }
 
 function displayAdditionQuestion(operand1, operand2){
+
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "+";
@@ -107,8 +112,12 @@ function displaySubtractQuestion(){
     
 }
 
-function displayMultiplyQuestion(){
-    
+function displayMultiplyQuestion(operand1, operand2){
+
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x";
+
 }
 
 
