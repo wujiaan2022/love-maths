9c6c9a9c3b11fde4ba01ef829bfb259ebfc08df3
wujiaan2022 <emmawu2004@gmail.com>
2023-01-16 01:45:54 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,22 +10,27 @@ document.addEventListener("DOMContentLoaded", function(){
                 alert("You clicked Submit!");
             }else{
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+    runGame("addition");
 })
 
-function runGame(){
+function runGame(gameType){
 
     // Create two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25 + 1);
     let num2 = Math.floor(Math.random() * 25 + 1);
-    
-}
-function runGame(){
-    
-}
+
+    if (gameType === "addition"){
+        displayAdditionQuestion(num1, num2);
+    }else{
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
+
+} 
 
 function checkAnswer(){
     
@@ -43,8 +48,10 @@ function incrementWrongAnswer(){
     
 }
 
-function displayAdditionQuestion(){
-    
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 }
 
 function displaySubtractQuestion(){
